613f5d531274372d257893593c8b2e52fef397b8
Woranan Hernandez <worananhernandez@Woranans-Air.lan>
2023-07-24 18:41:07 -0700

    updata

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
+import { Fragment } from 'react';
 import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const myBtn = () => {
+    console.log('updated');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>React App</h1>
+    <button onClick={ myBtn }></button>
+    <img src="https://static.vecteezy.com/system/resources/thumbnails/017/323/715/small/3d-render-adorable-close-up-of-a-american-eskimo-dog-holding-red-rose-in-mouth-on-white-background-photo.jpg"></img>
     </div>
   );
 }
